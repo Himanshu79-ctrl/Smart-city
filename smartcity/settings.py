446d1aebe294df430c59f4b34de23fe766a6c782
chat application <chat@gmail.com>
2026-05-09 11:00:37 +0530

    fix redirect server error

--- smartcity/settings.py
+++ smartcity/settings.py
@@ -183,8 +183,8 @@ STORAGES = {
 }
 
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = "profile"
-LOGOUT_REDIRECT_URL = "login"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/accounts/login/"
 
 
 SITE_ID = 1
